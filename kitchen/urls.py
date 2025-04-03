4c780cfdd8a493deaacbf9f6410a76fca63ff199
Roman Sachavskyi <roman.sachavskyi.ki.2019@lpnu.ua>
2025-04-03 22:02:12 +0300

    feat: add dish list page with view and URL

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from kitchen.views import index
+
+from kitchen.views import (
+    index,
+    DishListView
+)
+
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path("dish/", DishListView.as_view(), name="dish-list")
 ]
 
 app_name = "kitchen"
--- kitchen/views.py
+++ kitchen/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+
+from kitchen.models import Dish, DishType
 
 
 def index(request):
     return render(request, "kitchen/index.html")
+
+
+class DishListView(generic.ListView):
+    model = Dish
+    template_name = "kitchen/dish_list.html"
