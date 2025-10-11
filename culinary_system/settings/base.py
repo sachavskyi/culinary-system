14812028d04c014bdcc7ffb5d7056858995c06f0
Roman Sachavskyi <qpalzmxsw1@gmail.com>
2025-10-11 05:56:47 +0300

    refactor: split settings into dev and prod

--- culinary_system/settings/base.py
+++ culinary_system/settings/base.py
@@ -12,23 +12,19 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+from dotenv import load_dotenv
 
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECURE_KEY", "django-insecure-wsq-dy&!-jg2f!jmcshb-gq2y@g56knua03n*3)u_n3^u(j_!%")
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DJANGO_DEBUG", "") != "False"
-
-ALLOWED_HOSTS = ["127.0.0.1"]
-
-
-# Application definition
+SECRET_KEY = os.getenv("DJANGO_SECURE_KEY")
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -75,17 +71,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'culinary_system.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
